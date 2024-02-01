d5434cc42fe4949b7f13c3846348942fb3603f47
SouryaSparks <vzoid8@gmail.com>
2024-01-31 21:18:19 -0800

    commit

--- program.py
+++ program.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 import os
 import time
+import sys
+import matplotlib.pyplot as plt
+
 
 # This loads in the model
 
@@ -12,5 +15,19 @@ model_dir = os.path.join('models','model.h5')
 # The load_model() method takes in a pathway as an argument and loads the model if that path leads to a model 
 model = tf.keras.models.load_model(model_dir)
 
+# sys.argv allows arguments to be passed through the command prompt, the variable is a list containing two items, the second item of that list is the file name 
+args_passed = sys.argv
+image_name = args_passed[1]
+
+try:
+    img = cv2.imread(image_name)
+
+    # plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    # plt.show()
+
+    os.system(image_name)
+
+except:
+    Exception('image does not exist!')
 
 
